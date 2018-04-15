d8acd6428be11f2046d1183032b31d4b076e8a13
viogt <12400335+viogt@users.noreply.github.com>
2018-04-15 11:05:58 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 var http = require('http');
 
 http.createServer(function (req, res) {
-    var Mng = 'Mongo INFO:\nUser:' + process.env.MONGODB_USER + '\nPassword:' + process.env.MONGODB_PASSWORD + '\nDataBase:' + process.env.MONGODB_DATABASE;
+    var Mng;
+    try {
+        Mng = 'Mongo INFO:\nUser:' + process.env.MONGODB_USER + '\nPassword:' + process.env.MONGODB_PASSWORD + '\nDataBase:' + process.env.MONGODB_DATABASE;
+    } catch(e) { Mng = e.message; }
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(Mng);
 }).listen(8080, '0.0.0.0');
