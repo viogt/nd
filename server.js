1fa815060a71afcb5cdafe232a93679c2a2ec188
Viorel <viorel.topa@gmail.com>
2019-12-10 09:05:47 +0000

    Commit from OpenShift

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-var http = require('http');
+var http = require('http'), port = 8080;
 
 http.createServer(function (req, res) {
     var Mng;
@@ -8,4 +8,6 @@ http.createServer(function (req, res) {
     console.log(Mng);
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end(Mng);
-}).listen(8080);
+}).listen(port);
+
+console.log(`Running at ${port}`);
